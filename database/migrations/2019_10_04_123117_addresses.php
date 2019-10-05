d9e2c985af85339fd43a13ece8c626c7baa3e532
Stewart McIntosh <stewis@gmail.com>
2019-10-05 20:23:03 +0100

    Create migrations for addresses and restaurants

---
+++ database/migrations/2019_10_04_123117_addresses.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Addresses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('restaurant_id')->unsigned();
+            $table->foreign('restaurant_id')->references('id')->on('restaurants');
+            $table->string('address1');
+            $table->string('address2')->default(null)->nullable();
+            $table->string('town')->default(null)->nullable();
+            $table->string('postcode');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
