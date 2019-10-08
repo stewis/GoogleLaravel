2528fa894acc9235efcdc278a0f9e3b561fe45ee
Stewart McIntosh <stewis@gmail.com>
2019-10-08 21:17:28 +0100

    Add service provider for geocoding library

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         App\Providers\AddressModelServiceProvider::class,
+        App\Libraries\GeoCoding\GeoCodingServiceProvider::class
     ],
 
     /*
