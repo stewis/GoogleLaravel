7fa6f5e0d91492c106b7f9fc4fcc450e0f5e43e4
Stewart McIntosh <stewis@gmail.com>
2019-10-09 22:41:11 +0100

    Use a standard template

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -1,16 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>
-            @section('title') Search @show
-        </title>
-        <link rel="stylesheet" href="{{ secure_asset('css/map.css') }}">
-        <script src="https://kit.fontawesome.com/0ca262b039.js" crossorigin="anonymous"></script>
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-    </head>
-    <body>
-        <div id="map">
-        </div>
+@extends('layouts.default')
+
+@section('title')
+    Search
+@stop
+
+@section('content')
+    <div id="map">
+    </div>
+@endsection
+
+@push('scripts')
         <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
         <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_API') }}"></script>
         <script src="{{secure_asset('js/restaurantMap.js')}}">
@@ -29,5 +28,4 @@
                 );
             });
         </script>
-    </body>
-</html>
+@endpush
